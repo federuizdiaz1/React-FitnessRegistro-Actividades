6f0fb03473a4380e14873b77a18d4a97ac90fafa
fecherico <federicoruizdiaz91@gmail.com>
2021-03-17 13:50:57 -0300

    agregamos component Welcome y estilos

--- myfirstreact/src/componentes/Card.js
+++ myfirstreact/src/componentes/Card.js
@@ -1,20 +1,39 @@
 import React from 'react'
-import exerciseImg from '../images/exercise.png'
-import './estilos/Card.css'  
+//import exerciseImg from '../images/exercise.png'
+import circlesImg from '../images/circles.png'
+import './estilos/Card.css'
 
 class Card extends React.Component {
-
+    constructor(props) {
+        super(props)
+        this.state={
+            image:'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/bulbasaur.png?alt=media&token=567caf19-af47-414e-a9d4-3854ab24c7dc'
+        }
+    }
+    
+    componentDidMount(){
+        setTimeout(()=>{
+            this.setState({
+                image:'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06=b9c4b236-16a9-4a56-bba2-90c9660a0f06'
+            })
+        },5000)
+    }
     render() {
+        const { title, description, img, leftColor, rightColor } = this.props
         return (
-            <div className='card'>
-                <div>
-                    <div>
-                        <div>
-                            <img src={exerciseImg}></img>
+            <div className='card mx-auto Fitness-Card' style={{
+                backgroundImage: `url(${circlesImg}),linear-gradient(to right,${leftColor}, ${rightColor})`
+            }}
+            >
+
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={this.state.image} className="float-right"></img>
                         </div>
-                        <div>
-                            <h1>Technique Guides</h1>
-                            <p>Learn amazing street workout and calisthenics</p>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './componentes/Card'
+import Welcome from './componentes/Welcome'
 //import Exercises from './pages/Exercises'
 import 'bootstrap/dist/css/bootstrap.css'
 
@@ -8,4 +9,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card/>, container)
+ReactDOM.render(<div>
+    <Welcome  
+        username="Fede"
+    />
+    <Card
+        title="Technique Guides"
+        description="Learn amazing street workout and calisthenics"
+        img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+        leftColor="#A74CF2"
+        rightColor="#617BFB"
+
+    />
+</div>, container)
