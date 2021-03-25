703ba4bc3cba4275d9c72a7fcc6878c615f12d0c
fecherico <federicoruizdiaz91@gmail.com>
2021-03-25 11:57:20 -0300

    creacion boton y estilos

--- myfirstreact/src/componentes/AddButton.js
+++ myfirstreact/src/componentes/AddButton.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './estilos/AddButton.css'
 import buttonImg from '../images/add.png'
-//import {link} from 'react-router-dom'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
+
+
 
 const AddButton = () => (
+    <Link to="/ejercicio/nuevo">
+        <div>
+            <img src={buttonImg} className="Fitness-Add" alt="ejercicio" />
+        </div>
+    </Link>
 
-    <div>
-        <img src={buttonImg} className="Fitness-Add" alt="ejercicio" />
-    </div>
 
 )
 
--- myfirstreact/src/componentes/App.js
+++ myfirstreact/src/componentes/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { BrowserRouter,Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Ejercicio from '../paginas/Ejercicio'
 import EjercicioNuevo from '../paginas/EjercicioNuevo'
-
+import NotFound from '../paginas/NotFound'
 
 function App() {
     return (
         <BrowserRouter>
-            <Route path="/ejercicio" component={Ejercicio} />
-            <Route path="/ejercicio/nuevo" component={EjercicioNuevo} />
+            <Switch>
+                <Route exact path="/ejercicio" component={Ejercicio} />
+                <Route exact path="/ejercicio/nuevo" component={EjercicioNuevo} />
+                <Route component={NotFound} />
+            </Switch>
         </BrowserRouter>
     )
 }
