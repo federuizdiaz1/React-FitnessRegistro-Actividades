da3047a6badccbb056fdb7eb5babe3d3c16a9cc5
fecherico <federicoruizdiaz91@gmail.com>
2021-04-10 11:49:28 -0300

    creacion de formulario

--- myfirstreact/src/componentes/Card.js
+++ myfirstreact/src/componentes/Card.js
@@ -1,33 +1,32 @@
-import React from 'react'
+import React from "react";
 //import exerciseImg from '../images/exercise.png'
-import circlesImg from '../images/circles.png'
-import './estilos/Card.css'
+import circlesImg from "../images/circles.png";
+import "./estilos/Card.css";
 
 class Card extends React.Component {
- 
-    render() {
-        const { title, description, img, leftColor, rightColor } = this.props
-        return (
-            <div className='card mx-auto Fitness-Card' style={{
-                backgroundImage: `url(${circlesImg}),linear-gradient(to right,${leftColor}, ${rightColor})`
-            }}
-            >
-
-                <div className="card-body">
-                    <div className="row center">
-                        <div className="col-6">
-                            <img src={img} className="float-right"></img>
-                        </div>
-                        <div className="col-6 Fitness-Card-Info">
-                            <h1>{title}</h1>
-                            <p>{description}</p>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    const { title, description, img, leftColor, rightColor } = this.props;
+    return (
+      <div
+        className="card mx-auto Fitness-Card"
+        style={{
+          backgroundImage: `url(${circlesImg}),linear-gradient(to right,${leftColor}, ${rightColor})`,
+        }}
+      >
+        <div className="card-body">
+          <div className="row center">
+            <div className="col-6">
+              <img src={img} className="float-right"></img>
             </div>
-
-        )
-    }
+            <div className="col-6 Fitness-Card-Info">
+              <h1>{title}</h1>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Card
+export default Card;
--- myfirstreact/src/paginas/EjercicioNuevo.js
+++ myfirstreact/src/paginas/EjercicioNuevo.js
@@ -1,90 +1,37 @@
-import React from 'react'
+import React from "react";
+import EjercicioForm from "../componentes/EjercicioForm";
+import Card from "../componentes/Card";
 
-class EjercicioNuevo extends React.Component{
+class EjercicioNuevo extends React.Component {
+  state = {
+    form: {
+      title: '',
+      description: '',
+      img: '',
+      leftColor: '',
+      rightColor: '',
+    },
+  };
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
-
-    state = {}
-
-    handleSubmit = e => {
-        e.preventDefault()
-        console.log(this.state)
-    }
-
-    handleChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-
-
-    render(){
+  render() {
     return (
-        <div className="container">
-            <form 
-                onSubmit={this.handleSubmit}
-            >
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="title" 
-                        name="title"
-                        onChange={this.handleChange}
-                        value={this.state.title}
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="description" 
-                        name="description"
-                        onChange={this.handleChange}
-                        value={this.state.description}
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="img" 
-                        name="img"
-                        onChange={this.handleChange}
-                        value={this.state.img}
-                    />
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder="leftColor" 
-                            name="leftColor"
-                            onChange={this.handleChange}
-                            value={this.state.leftColor}
-                        />
-                    </div>
-                    <div className="col">
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            placeholder="rightColor" 
-                            name="rightColor"
-                            onChange={this.handleChange}
-                            value={this.state.rightColor}
-                        />    
-                    </div>
-                </div>
-                
-                <button 
-                    type="submit" 
-                    className="btn btn-primary"
-                >
-                    Submit
-                </button>
-            </form>
+      <div className="row">
+        <div className="col-sm">
+          <Card {...this.state.form} />
         </div>
-    )
+        <div className="col-sm">
+          <EjercicioForm onChange={this.handleChange} form={this.state.form} />
+        </div>
+      </div>
+    );
+  }
 }
-}
-export default EjercicioNuevo
+export default EjercicioNuevo;
